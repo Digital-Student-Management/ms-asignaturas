2d4206d4465ac3a339383883b2c3cb44a1d2a6ec
Darío Meza <dari.meza@duocuc.cl>
2026-05-04 12:39:00 -0400

    chore: initialize Spring Boot project with Maven Wrapper and base configuration

---
+++ ms-asignaturas/src/main/java/com/ms_sistemaEscolar/ms_asignaturas/MsAsignaturasApplication.java
@@ -0,0 +1,13 @@
+package com.ms_sistemaEscolar.ms_asignaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAsignaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAsignaturasApplication.class, args);
+	}
+
+}
