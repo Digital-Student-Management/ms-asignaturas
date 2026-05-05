e27c7b19b95b4470866129ceb5aaa2cc9e09308b
Darío Meza <dari.meza@duocuc.cl>
2026-05-04 22:58:10 -0400

    feat: initialize ms-asignaturas microservice with project structure, dependencies, and BitacoraAsignatura repository

--- ms-asignaturas/src/main/java/com/ms_sistemaEscolar/ms_asignaturas/repositories/BitacoraAsignaturaRepository.java
+++ ms-asignaturas/src/main/java/com/ms_sistemaEscolar/ms_asignaturas/repositories/BitacoraAsignaturaRepository.java
@@ -2,6 +2,8 @@ package com.ms_sistemaEscolar.ms_asignaturas.repositories;
 
 import com.ms_sistemaEscolar.ms_asignaturas.models.entity.BitacoraAsignatura;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,7 @@ import java.util.List;
 @Repository
 public interface BitacoraAsignaturaRepository extends JpaRepository<BitacoraAsignatura, Integer> {
 
-    List<BitacoraAsignatura> findByAsignatura_IdAsignatura(int id_asignatura);
+    @Query("SELECT b FROM BitacoraAsignatura b WHERE b.asignatura.id_asignatura = :idAsignatura")
+    List<BitacoraAsignatura> findByAsignatura_IdAsignatura(@Param("idAsignatura") int id_asignatura);
 
 }
